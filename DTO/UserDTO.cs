2bcaa158d518fb43daae94c64f8124442d983975
Yehudit Kaufman <38214982472@mby.co.il>
2025-03-04 19:15:40 +0200

    project finish!!

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -6,3 +6,4 @@ namespace DTO
     public record UserDTO(int UserId, [EmailAddress] string UserName, [StringLength(20, ErrorMessage = "LastName can be betwenn 2 till 20 letters", MinimumLength = 2)] string FirstName, [StringLength(20, ErrorMessage = "LastName can be betwenn 2 till 20 letters", MinimumLength = 2)] string LastName);
     public record RegisterUserDTO([EmailAddress] string UserName, [StringLength(20, ErrorMessage = "FirstName can be betwenn 2 till 20 letters", MinimumLength = 2),]string FirstName, [StringLength(20, ErrorMessage = "LastName can be betwenn 2 till 20 letters", MinimumLength = 2)]string LastName, string Password);
 }
+//
